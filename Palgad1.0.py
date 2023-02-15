1a53dbbd2cfb426f45e03a902be068a339f5210e
milju <milju@MSI>
2023-02-15 08:32:10 +0200

    all

--- Palgad1.0.py
+++ Palgad1.0.py
@@ -8,7 +8,7 @@ inimesed=["A","B","C","D","A"]
 while True:
     print(inimesed)
     print(palgad)
-    menu=int(input("Valik:\n 1-Lisa andmed\n 2-Kustuta andmed\n 3-Suurim palk\n 4-Väiksem palk\n 5-Vordses palgad\n 12-Sort\n 13-Kustuta "))
+    menu=int(input("Valik:\n 1-Lisa andmed\n 2-Kustuta andmed\n 3-Suurim palk\n 4-Väiksem palk\n 5-Vordses palgad\n 7-Nimes\n 8-Suur väike\n 9-top3\n 10-Keskmine palk\n 11-Tulumaks\n 12-Sort\n 13-Kustuta "))
     if menu==0:
         break
     elif menu==1:
@@ -27,8 +27,20 @@ while True:
         inimesed,palgad=Vordses_palgad(inimesed,palgad)
         print(inimesed)
         print(palgad)
+    elif menu==7:
+        inimesed,palgad=nimes(inimesed,palgad)
+    elif menu==8:
+        inimesed,palgad=suur_väike(inimesed,palgad)
+    elif menu==9:
+        inimesed,palgad=top3(inimesed,palgad)
+    elif menu==10:
+        inimesed,palgad=keskminepalk(inimesed,palgad)
+    elif menu==11:
+            tulumaks()
     elif menu==12:
         inimesed,palgad=Sorteerimine(inimesed,palgad)
     elif menu==13:
         inimesed,palgad=Kustuta(inimesed,palgad)
         print(inimesed,palgad)
+
+
--- module1.py
+++ module1.py
@@ -83,6 +83,22 @@ def Sorteerimine(i:list,p:list):
     return i,p
 
 
+def suur_väike(i:list,p:list):
+
+    number=int(input("Naita palga number"))
+    v=int(input("Kellel palk 1-on suurem,2-on väiksem?"))
+    if v==1:
+        for g in p:
+            if g > number:
+                ind=p.index(g)
+                print(f"{g}, {i[ind]}")
+    if v==2:
+        for g in p:
+            if g < number:
+                ind=p.index(g)
+                print(f"{g}, {i[ind]}")
+    return i,p
+
 def Vordses_palgad(i:list,p:list):
     """
     :param list i:Inimeste järjend
@@ -133,3 +149,71 @@ def sort_a_b(i:list,p:list):
     data = ['Elle', 'Miles', 'Kratos', 'Joel', 'Peter', 'Nathan']
 
     print(sorted(data))
+
+def top3(i:list,p:list):
+    """
+    """
+    top1=p[0]
+    top2=p[0]
+    top3=p[0]
+    for palk in p:
+        if palk>top1:
+            top1=palk
+        elif palk>top2:
+            top2=palk
+        elif palk>top3:
+            top3=palk
+    print("rikkad",top1, top2, top3)
+    top1=p[0]
+    top2=p[0]
+    top3=p[0]
+    for palk in p:
+        if palk<top1:
+            top1=palk
+        elif palk<top2:
+            top2=palk
+        elif palk<top3:
+            top3=palk
+
+    print("vaesed",top1, top2, top3)
+    return i,p
+
+def keskminepalk(i:list,p:list):
+    kesk_palk=sum(p)/len(p)
+    for palk in p:
+        if palk==kesk_palk:
+            ind=p.index(palk)
+            print(f"{kesk_palk},{i[ind]}")
+    return i,p
+
+def tulumaks():
+    """
+    """
+    tulu=int(input("palga suurus "))
+    if tulu<1200:
+        maksuvaba_tulu=500
+    elif tulu<2100:
+        maksuvaba_tulu=500-0.55556(tulu-1200)
+    else:
+        maksuvaba_tulu=0
+    maks=(tulu-maksuvaba_tulu)*0.20
+    netopalk=tulu-maks
+    print(f"netopalk on {netopalk}")
+
+def nimes(i:list,p:list):
+    """
+    """
+    nimi=input("kelle tahad leida")
+    while nimi not in i:
+        nimi=input("palun kirjuta õige nimi")
+        n=i.count(nimi)
+    if n!=1:
+        print(f"siin on mõned inimesed kes nimi on {nimi}")
+        kopia=i.copy()
+        for i in range(n):
+            ind=kopia.index(nimi)
+            kopia.remove(nimi)
+            kopia.insert(ind,"")
+            print(f"{i+1} {nimi} saab {p[ind]}")
+
+    return i,p
